502e4b4ce0ca7ad16a38060ab6899bf47aefc464
Anzhalika Turlak <turlak.by@gmail.com>
2023-12-25 18:37:02 +0300

    feat: done task vigenere shifr

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -25,12 +25,57 @@ class VigenereCipheringMachine {
       throw new Error('Incorrect arguments!');
     }
 
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let countAlphabet = 26;
+    let codeSymbolA = 'A'.charCodeAt(0);
+
+    let result = [];
+    let keyI = 0;
+
+    message.split('').forEach((item, index) => {
+      if (item.match(/[A-Z]/)) {
+        let indexSymbol = message.charCodeAt(index) - codeSymbolA;
+        let shift = key.charCodeAt(keyI % key.length) - codeSymbolA;
+
+        result.push(String.fromCharCode(codeSymbolA + (indexSymbol + shift) % countAlphabet));
+        keyI++;
+      } else {
+        result.push(item);
+      }
+    });
+
+    return this.reverseCode ? result.join('') : result.reverse().join('');
+
   }
   decrypt(encryptedMessage, key) {
-    if(!message || ! key) {
+    if(!encryptedMessage || ! key) {
       throw new Error('Incorrect arguments!');
     }
- 
+    
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let countAlphabet = 26;
+    let codeSymbolA = 'A'.charCodeAt(0);
+
+    let result = [];
+    let keyI = 0;
+
+    encryptedMessage.split('').forEach((item, index) => {
+      if (item.match(/[A-Z]/)) {
+        let indexSymbol = encryptedMessage.charCodeAt(index) - codeSymbolA;
+        let shift = key.charCodeAt(keyI % key.length) - codeSymbolA;
+
+        const charCode = (indexSymbol - shift + countAlphabet) % countAlphabet + codeSymbolA;
+        result.push(String.fromCharCode(charCode));
+        keyI++;
+      }
+      else result.push(item);
+    });
+
+    return this.reverseCode ? result.join('') : result.reverse().join('');
   }
 }
 
